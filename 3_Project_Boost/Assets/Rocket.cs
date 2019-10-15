53a82459780dab9ec08737a2d4d4b0837406d84a
adeick <37886715+adeick@users.noreply.github.com>
2019-10-15 09:58:44 -0500

    Added Obstacles
    Initial Steps toward Collisions
    Added 3 Obstacles
    Added Landing Pad
    Updated Rocket Size and Camera Position
    10/15/2019

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -4,31 +4,62 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-
+    [SerializeField] float rotationalThrust = 100f;
+    [SerializeField] float forwardThrust = 1000f;
+    
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetInput();
+        Thrust();
+        Rotate();
     }
-    
-    void GetInput(){
-        if(Input.GetKey(KeyCode.Space)){
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+    void Thrust(){
+        float rotationThisFrame = forwardThrust * Time.deltaTime;
 
+        if(Input.GetKey(KeyCode.Space)){
+            rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+        }
+        else{
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate(){
+        float rotationThisFrame = rotationalThrust * Time.deltaTime;
+
+        rigidBody.freezeRotation = true; // stop Physics so we can handle rocket
         if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false; //Resume Physics
+    }
+
+    void OnCollisionEnter(Collision collision){
+        switch(collision.gameObject.tag){
+            case "Friendly":
+                print("OK");
+                break;
+            case "Danger":
+                print("Dead");
+                break;
         }
     }
+
 }
+
