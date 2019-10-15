e7c295b17d629d2aba8594e29df3e85c168154b1
adeick <37886715+adeick@users.noreply.github.com>
2019-10-15 16:52:51 -0500

    Added Audio Clips, Prefabs, and Particle Effects
    Audio Clips Complete
    Prefab complete
    Particle Effects Recently Imported
    10/15/2019

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rotationalThrust = 100f;
     [SerializeField] float forwardThrust = 1000f;
-    
+    [SerializeField] AudioClip engineSound;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip levelUp;
+     
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+enum State {Alive, Dead, Transcending}
+State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,42 +30,65 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
     void Thrust(){
-        float rotationThisFrame = forwardThrust * Time.deltaTime;
-
-        if(Input.GetKey(KeyCode.Space)){
-            rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
-            if(!audioSource.isPlaying){
-                audioSource.Play();
+        if(state == State.Alive){
+            float rotationThisFrame = forwardThrust * Time.deltaTime;
+            if(Input.GetKey(KeyCode.Space)){
+                rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
+                if(!audioSource.isPlaying){
+                    audioSource.PlayOneShot(engineSound);
+                }
             }
-        }
-        else{
-            audioSource.Stop();
+            else{
+                audioSource.Stop();
+            }
         }
     }
 
     void Rotate(){
-        float rotationThisFrame = rotationalThrust * Time.deltaTime;
-
-        rigidBody.freezeRotation = true; // stop Physics so we can handle rocket
-        if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.forward * rotationThisFrame);
+        if(state == State.Alive){
+            float rotationThisFrame = rotationalThrust * Time.deltaTime;
+            rigidBody.freezeRotation = true; // stop Physics so we can handle rocket
+            if(Input.GetKey(KeyCode.A)){
+                transform.Rotate(Vector3.forward * rotationThisFrame);
+            }
+            else if(Input.GetKey(KeyCode.D)){
+                transform.Rotate(-Vector3.forward * rotationThisFrame);
+            }
+            rigidBody.freezeRotation = false; //Resume Physics
         }
-        else if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(-Vector3.forward * rotationThisFrame);
-        }
-        rigidBody.freezeRotation = false; //Resume Physics
     }
 
     void OnCollisionEnter(Collision collision){
-        switch(collision.gameObject.tag){
-            case "Friendly":
-                print("OK");
-                break;
-            case "Danger":
-                print("Dead");
-                break;
+        if(state == State.Alive){
+            switch(collision.gameObject.tag){
+                case "Friendly":
+                    print("OK");
+                    break;
+                case "Finish":
+                    print("Finished");
+                    state = State.Transcending;
+                    audioSource.PlayOneShot(levelUp);
+                    Invoke("LoadNextScene", 1f);
+                    break;
+                case "Danger":
+                    print("Dead");
+                    state = State.Dead;
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(death);
+                    Invoke("LoadNextScene", 1f);
+                    break;
+            }
         }
     }
 
+    void LoadNextScene(){
+        if(state == State.Transcending){
+            SceneManager.LoadScene(1);
+        }
+        else if(state == State.Dead){
+            SceneManager.LoadScene(0);
+        }
+        state = State.Alive;
+    }
 }
 
