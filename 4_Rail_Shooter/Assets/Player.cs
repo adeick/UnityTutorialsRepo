8d4aa1f10526d546904d148ecf6ef2476dd40c99
adeick <37886715+adeick@users.noreply.github.com>
2019-10-16 23:06:15 -0500

    New Terrain, Rotations, and Particles
    Created a larger path, jet now tilts on movement, and introduced particles (with the intention of lasers)

---
+++ 4_Rail_Shooter/Assets/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float ySpeed = 4f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = -5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TranslationMovement();
+        RotationalMovement();
+
+    }
+    void TranslationMovement(){
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        rawNewXPos = Mathf.Clamp(rawNewXPos, -2.2f, 2.2f);
+        rawNewYPos = Mathf.Clamp(rawNewYPos, -1.5f, 1.5f);
+
+        transform.localPosition = new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z);
+    }
+
+    void RotationalMovement(){
+
+        float pitch = transform.localPosition.y * positionPitchFactor;
+
+        float yaw = transform.localPosition.x * positionYawFactor;;
+        float roll = 0f;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
