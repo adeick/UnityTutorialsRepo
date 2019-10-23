1d2970b75409b9d5798721fc592e870b0072e65a
adeick <37886715+adeick@users.noreply.github.com>
2019-10-23 15:44:37 -0500

    Score Text
    Score now updates when an enemy is killed.
    Score can be assigned differently to different enemies.

--- 4_Rail_Shooter/Assets/Enemy.cs
+++ 4_Rail_Shooter/Assets/Enemy.cs
@@ -6,10 +6,13 @@ public class Enemy : MonoBehaviour
 {
     [Tooltip("Explosion Particle Effect")][SerializeField] GameObject explosionFX; 
     // Start is called before the first frame update
+    [SerializeField] int killPoints = 12;
+    ScoreBoard scoreBoard;
     void Start()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     // Update is called once per frame
@@ -20,5 +23,6 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other){
         Instantiate(explosionFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        scoreBoard.ScoreHit(killPoints);
     }
 }
