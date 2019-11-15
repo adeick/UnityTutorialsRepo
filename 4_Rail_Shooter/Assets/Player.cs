4fa9a1c44220e6d738badd590edfec1787d4e979
adeick <37886715+adeick@users.noreply.github.com>
2019-11-14 23:47:28 -0600

    Readded Light to Sandbox House
    11/14/2019
    
    Side note, it seems as if some meta files from Project 4 (Rail Shooter) have changed...

---
+++ 4_Rail_Shooter/Assets/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [Group("General")]
+    [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
+    [Tooltip("In ms^-1")][SerializeField] float xRange = 10f;
+    [Tooltip("In ms^-1")][SerializeField] float yRange = 9f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -25f;
+    [SerializeField] float positionYawFactor = 6f;
+    [SerializeField] float controlRollFactor = -25f;
+
+    float xThrow, yThrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TranslationMovement();
+        RotationalMovement();
+
+    }
+    void TranslationMovement(){
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        rawNewXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+        rawNewYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z);
+    }
+
+    void RotationalMovement(){
+        float pitch, yaw, roll;
+        float controlPitch;
+
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor ;
+        yaw = transform.localPosition.x * positionYawFactor;
+
+        if(xRange - Mathf.Abs(transform.localPosition.x) < 0.1){
+            roll = controlRollFactor * (xRange - Mathf.Abs(transform.localPosition.x));
+        }
+        else{
+            roll = xThrow * controlRollFactor;
+        }
+        if(yRange - Mathf.Abs(transform.localPosition.y) < 0.1){
+            controlPitch = controlPitchFactor * (yRange - Mathf.Abs(transform.localPosition.y));
+        }
+        else{
+            controlPitch = yThrow * controlPitchFactor;
+        }
+        pitch = pitchDueToPosition + controlPitch;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
