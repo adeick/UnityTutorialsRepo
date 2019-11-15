651a31cdc0e1efba04b4c85f2bc1a6d95e51a213
adeick <37886715+adeick@users.noreply.github.com>
2019-11-15 01:31:47 -0600

    Large Enemy Overhaul
    Enemy has stopping distance of 2.5, (at which point console prints an attack), otherwise it will follow you.
    Enemy now has hitpoints, and if it loses all of them, it will self-destruct.
    
    Gave player a gun, and gun shoots RayCasts to deal damage to the enemy. Also added Muzzle Flash.
    11/15/2019

---
+++ 6_Zombie_Runner/Zombie Runner/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int healthPoints = 100;
+
+    public void TakeDamage(int damage){
+        healthPoints -= damage;
+        if(healthPoints < 0){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 6_Zombie_Runner/Zombie Runner/Assets/Scripts/Weapon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField] Camera FPCamera;
+    [SerializeField] ParticleSystem MuzzleFlash;
+    [SerializeField] float range = 100f;
+    [SerializeField] int damage = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        ProcessRaycast();
+        PlayMuzzleFlash();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            print("You missed.");
+            return;
+        }
+    }
+    private void PlayMuzzleFlash(){
+        MuzzleFlash.Play();
+    }
+}
