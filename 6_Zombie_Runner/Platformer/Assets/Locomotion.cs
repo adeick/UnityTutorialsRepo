8ced4d8d126b14f588ede48a5730b849af0024cc
adeick <37886715+adeick@users.noreply.github.com>
2020-01-08 22:53:22 -0600

    Assorted Changes
    Computer decided to change stuff

---
+++ 6_Zombie_Runner/Platformer/Assets/Locomotion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetAxis("Vertical") > 0){
+            anim.SetTrigger("jump");
+            anim.SetBool("crouch", false);
+        }
+        else if (Input.GetAxis("Vertical") < 0){
+            anim.SetBool("crouch", true);
+        }
+        else{
+            anim.SetBool("crouch", false);
+        }
+        
+        if(Input.GetAxis("Horizontal") > 0){
+            anim.SetBool("forward", true);
+            anim.SetBool("back", false);
+        }
+        else if(Input.GetAxis("Horizontal") < 0){
+            anim.SetBool("forward", false);
+            anim.SetBool("backward", true);
+        }
+        else {
+            anim.SetBool("forward", false);
+            anim.SetBool("backward", false);
+        }
+    }
+}
